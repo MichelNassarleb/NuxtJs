b5a2e2cc4d77fa3ed51d8e6c9895d8226a91b1d5
MichelNassarleb <michelnassar371@gmail.com>
2024-05-30 14:14:38 +0300

    added layouts

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <!-- NuxtLink gives you the ability to navigate to new screens without making extra requests to the server if it's not needed
+              which makes your web app much more performant and scalable-->
+  
+              <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/about">About</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/products">Products</NuxtLink>
+            </li> 
+          </ul>
+        </nav>
+      </header>
+      <div>
+        <slot />
+      </div>
+      </div>
+  </template>
+  
+  <script setup></script>
+  <style scoped>
+.router-link-exact-active {
+    color:aqua;
+}
+  </style>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,5 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <!-- NuxtLink gives you the ability to navigate to new screens without making extra requests to the server if it's not needed
-which makes your web app much more performant and scalable-->
-
-            <NuxtLink to="/">Home</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="products/">Products</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="products/">Products</NuxtLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
     <h2>Home</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eius ex,
