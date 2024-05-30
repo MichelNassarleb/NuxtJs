3fcbcee7b14ea5af1db9f63068125407fb350191
MichelNassarleb <michelnassar371@gmail.com>
2024-05-30 12:39:25 +0300

    added project initial setup

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,20 @@
+
+import { updateAppConfig } from '#app/config'
+import { defuFn } from 'defu'
+
+const inlineConfig = {
+  "nuxt": {
+    "buildId": "e266682d-d3ed-43cd-bc8d-5324d1b129fd"
+  }
+}
+
+// Vite - webpack is handled directly in #app/config
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    updateAppConfig(newModule.default)
+  })
+}
+
+
+
+export default /*@__PURE__*/ defuFn(inlineConfig)
